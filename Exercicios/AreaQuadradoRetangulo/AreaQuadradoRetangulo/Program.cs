c4949ce920cfb113d092cfcd73f74fb6f7d3b6a7
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-04-11 11:25:04 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcula a área quadrado/retângulo");
+
+            Area area = new Area();
+
+            Console.WriteLine(area.Calcular());
+
+            Console.WriteLine(area.Dados());
+        }
+    }
+}
---
+++ Exercicios/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Giu";
+            p.DataNascimento = new DateTime(2007, 8, 25);
+            p.Altura = 1.60f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+
+            Console.ReadKey();
+        }
+
+    }
+}
