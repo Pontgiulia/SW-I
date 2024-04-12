49e5ccadf20be41f60e9e31acf0b03844b3d6049
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-04-12 11:44:22 -0300

    Add files via upload

---
+++ Exercicios/ExercicioSalario/ExercicioSalario/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExercicioSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario p = new Salario();
+            p.Nome = "Giu";
+            p.Sal = 20000;
+            p.HED = 0;
+            p.HEN = 24;
+            p.ND = 0;
+            p.Fal = 32;
+            p.DE = 5;
+            p.REF = 2;
+            p.Val = 0;
+            Console.WriteLine(p.FolhaDePagamento());
+        }
+    }
+}
