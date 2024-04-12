b9fac49af88a96ea59470be344ca799fc030432f
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-04-12 11:31:49 -0300

    Add files via upload

---
+++ Exercicios/fotos/fotos/AlbumDeFotos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fotos
+{
+    public class AlbumDeFotos
+    {
+        public int TotalDePaginas { get; set; }
+
+        public int TotalDeFotos { get; set; }
+
+        public int NumeroDeFotos
+        {
+            get
+            {
+                return NumeroDeFotos;
+            }
+            set
+            {
+                if (value > TotalDeFotos)
+                {
+                    Console.WriteLine("Você passou o limite de fotos");
+                }
+            }
+        }
+    }
+}
---
+++ Exercicios/fotos/fotos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Fotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlbumDeFotos fotas1 = new AlbumDeFotos();
+            Console.WriteLine("Situação 1");
+            fotas1.TotalDeFotos = 40;
+            fotas1.NumeroDeFotos = 60;
+            fotas1.TotalDePaginas = 30;
+            Console.WriteLine("Limite de Fotos:40,Quantia de Fotos:60");
+
+            AlbumDeFotos fotas2 = new AlbumDeFotos();
+            Console.WriteLine("Situação 2");
+            fotas1.TotalDeFotos = 96;
+            fotas1.NumeroDeFotos = 20;
+            fotas1.TotalDePaginas = 30;
+            Console.WriteLine("Limite de Fotos:20,Quantia de Fotos:96");
+        }
+    }
+}
