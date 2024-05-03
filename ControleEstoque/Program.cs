5125096a8f1b12c5a87e4c5e88de938f0ad093ee
Giulia Pontelli <124791767+Pontgiulia@users.noreply.github.com>
2024-05-03 12:41:06 -0300

    Add files via upload

---
+++ ControleEstoque/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ControleEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(20, "Sukuna_fingers", 0, 20000);
+            p.imprimir();
+            p.entrada(20);
+            p.imprimir();
+            p.venda(5);
+            p.imprimir();
+        }
+    }
+}
